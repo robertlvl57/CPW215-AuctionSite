a8dffac89ae031bd4e264202a7caa9a75b3311c5
robert lowry <lowryrob2001@gmail.com>
2019-10-23 14:42:46 -0700

    Added HomeController test

---
+++ AuctionSiteTests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AuctionSite.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuctionSite.Controllers.Tests
+{
+    [TestClass()]
+    public class HomeControllerTests
+    {
+        [TestMethod()]
+        public void Index_ReturnViewResult()
+        {
+            HomeController home = new HomeController();
+
+            var result = home.Index();
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
